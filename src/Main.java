5ee079db29dbe9c9445aa6038a278907963aea47
John Blyatson <48108297+bleenMachine@users.noreply.github.com>
2020-08-10 22:56:54 -0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        AreaCalculator.area(5.0); //78.53975
+        AreaCalculator.area(-1); // -1 so no output
+        AreaCalculator.area(5.0, 4.0); //20.0
+        AreaCalculator.area(-1.0, 4.0); // -1 so no output
+    }
+}
